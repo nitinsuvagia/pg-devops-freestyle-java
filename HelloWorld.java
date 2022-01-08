d232c3c3dd7136005544b95ac3265ea0e9e6acf1
Nitin Suvagiya <nitinsoftqubes@ip-172-31-93-140.ec2.internal>
2022-01-08 18:01:37 +0000

    new updates

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World, to triiger auto while push");
+		System.out.println("Hello, World, to triiger auto push");
 	}
 }
